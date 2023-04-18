faae0728b350680c00c7f641bf7996d358b14557
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2023-04-18 18:41:44 -0500

    run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+accelerate launch pretrain_e4t.py \
+  --mixed_precision="fp16" \
+  --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
+  --domain_class_token="art" \
+  --placeholder_token="*s" \
+  --prompt_template="art" \
+  --save_sample_prompt="a photo of the *s,a photo of the *s in monet style" \
+  --reg_lambda=0.01 \
+  --domain_embed_scale=0.1 \
+  --output_dir="pretrained-wikiart" \
+  --train_image_dataset="Artificio/WikiArt" \
+  --iterable_dataset \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --learning_rate=1e-6 --scale_lr \
+  --checkpointing_steps=10000 \
+  --log_steps=1000 \
+  --max_train_steps=100000 \
+  --unfreeze_clip_vision \
+  --enable_xformers_memory_efficient_attention 

bdd6f313dae069f23076b61093cc99bc75b45814
DESKTOP-FP8LE75\coold <cooldude242@gmail.com>
2023-04-23 10:41:10 -0500

    updates

--- inference.sh
+++ inference.sh
@@ -1,15 +1,15 @@
 INPUT_PATH=$1
 PROMPT=$2
 INPUT_PATH="/home/ubuntu/e4t-diffusion/training_images/$INPUT_PATH"
-PROJECT="aes65"
+PROJECT="diffusiondb"
 
 echo "Prompt: $PROMPT"
 echo "Input path: $INPUT_PATH"
 
 python inference.py \
-  --pretrained_model_name_or_path "./output/$PROJECT/30/" \
+  --pretrained_model_name_or_path "./output/$PROJECT/100/" \
   --prompt "$PROMPT" \
-  --num_images_per_prompt 3 \
+  --num_images_per_prompt 8 \
   --scheduler_type "ddim" \
   --output_dir "./output/$PROJECT" \
   --image_path_or_url="$INPUT_PATH" \
--- tuning.sh
+++ tuning.sh
@@ -1,15 +1,16 @@
 INPUT_PATH=$1
 INPUT_PATH="/home/ubuntu/e4t-diffusion/training_images/$INPUT_PATH"
-PROJECT="aes65"
+PROJECT="diffusiondb"
 
 accelerate launch tuning_e4t.py \
-  --pretrained_model_name_or_path="/home/ubuntu/e4t-diffusion/pretrained-aesthetics/150000/" \
-  --reg_lambda=0.1 \
+  --pretrained_model_name_or_path="/home/ubuntu/e4t-diffusion/pretrained-diffusiondb/100000/" \
+  --reg_lambda=1e-4 \
   --output_dir "./output/$PROJECT" \
   --train_image_path="$INPUT_PATH" \
   --resolution=256 \
   --train_batch_size=4 \
-  --learning_rate=1e-6 --scale_lr \
-  --max_train_steps=30 \
+  --learning_rate=1e-6 \
+  --scale_lr \
+  --max_train_steps=100 \
   --mixed_precision="fp16" \
   --enable_xformers_memory_efficient_attention
